6f39c639df50b3e28c0a06ec6e870e1739810f20
LinKun <linkun199011@163.com>
2016-11-14 15:57:55 +0800

    init2

--- app/src/main/java/com/ustclin/petchicken/utils/HttpUtils.java
+++ app/src/main/java/com/ustclin/petchicken/utils/HttpUtils.java
@@ -13,118 +13,103 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 
-public class HttpUtils
-{
-	public static String API_KEY = "b93b2d9c05a91658145d2431f3f18168";
-	private static String URL = "http://www.tuling123.com/openapi/api";
+public class HttpUtils {
+    public static String API_KEY = "b93b2d9c05a91658145d2431f3f18168";
+    private static String URL = "http://www.tuling123.com/openapi/api";
 
-	/**
-	 * 发送一个消息，并得到返回的消息
-	 * @param msg
-	 * @return
-	 */
-	public static ChatMessage sendMsg(String msg)
-	{
-		ChatMessage message = new ChatMessage();
-		String url = setParams(msg);
-		String res = doGet(url);
-		Gson gson = new Gson();
-		Result result = gson.fromJson(res, Result.class);
-		
-		if (result.getCode() > 400000 || result.getText() == null
-				|| result.getText().trim().equals(""))
-		{
-			message.setMsg("主人，我的爸爸还没有开发出这个功能呢，你问我，我也很为难呢。。");
-		}else
-		{
-			message.setMsg(result.getText());
-			//
-		}
-		message.setType(ChatMessage.MESSAGE_IN);
-		message.setDate(MyDateUtils.getDate());
-		
-		return message;
-	}
+    /**
+     * 发送一个消息，并得到返回的消息
+     *
+     * @param msg
+     * @return
+     */
+    public static ChatMessage sendMsg(String msg) {
+        ChatMessage message = new ChatMessage();
+        String url = setParams(msg);
+        String res = doGet(url);
+        Gson gson = new Gson();
+        Result result = gson.fromJson(res, Result.class);
 
-	/**
-	 * 拼接Url
-	 * @param msg
-	 * @return
-	 */
-	private static String setParams(String msg)
-	{
-		try
-		{
-			msg = URLEncoder.encode(msg, "UTF-8");
-		} catch (UnsupportedEncodingException e)
-		{
-			e.printStackTrace();
-		}
-		return URL + "?key=" + API_KEY + "&info=" + msg;
-	}
+        if (result.getCode() > 400000 || result.getText() == null
+                || result.getText().trim().equals("")) {
+            message.setMsg("主人，我的爸爸还没有开发出这个功能呢，你问我，我也很为难呢。。");
+        } else {
+            message.setMsg(result.getText());
+            //
+        }
+        message.setType(ChatMessage.MESSAGE_IN);
+        message.setDate(MyDateUtils.getDate());
 
-	/**
-	 * Get请求，获得返回数据
-	 * @param urlStr
-	 * @return
-	 */
-	private static String doGet(String urlStr)
-	{
-		URL url = null;
-		HttpURLConnection conn = null;
-		InputStream is = null;
-		ByteArrayOutputStream baos = null;
-		try
-		{
-			url = new URL(urlStr);
-			conn = (HttpURLConnection) url.openConnection();
-			conn.setReadTimeout(5 * 1000);
-			conn.setConnectTimeout(5 * 1000);
-			conn.setRequestMethod("GET");
-			if (conn.getResponseCode() == 200)
-			{
-				is = conn.getInputStream();
-				baos = new ByteArrayOutputStream();
-				int len = -1;
-				byte[] buf = new byte[128];
+        return message;
+    }
 
-				while ((len = is.read(buf)) != -1)
-				{
-					baos.write(buf, 0, len);
-				}
-				baos.flush();
-				return baos.toString();
-			} else
-			{
-				throw new CommonException("主人，我找不到服务器啦~");
-			}
+    /**
+     * 拼接Url
+     *
+     * @param msg
+     * @return
+     */
+    private static String setParams(String msg) {
+        try {
+            msg = URLEncoder.encode(msg, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return URL + "?key=" + API_KEY + "&info=" + msg;
+    }
 
-		} catch (Exception e)
-		{
-			e.printStackTrace();
-			throw new CommonException("主人，我找不到服务器啦~");
-		} finally
-		{
-			try
-			{
-				if (is != null)
-					is.close();
-			} catch (IOException e)
-			{
-				e.printStackTrace();
-			}
+    /**
+     * Get请求，获得返回数据
+     *
+     * @param urlStr
+     * @return
+     */
+    private static String doGet(String urlStr) {
+        URL url = null;
+        HttpURLConnection conn = null;
+        InputStream is = null;
+        ByteArrayOutputStream baos = null;
+        try {
+            url = new URL(urlStr);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(5 * 1000);
+            conn.setConnectTimeout(5 * 1000);
+            conn.setRequestMethod("GET");
+            if (conn.getResponseCode() == 200) {
+                is = conn.getInputStream();
+                baos = new ByteArrayOutputStream();
+                int len = -1;
+                byte[] buf = new byte[128];
 
-			try
-			{
-				if (baos != null)
-					baos.close();
-			} catch (IOException e)
-			{
-				e.printStackTrace();
-			}
-			conn.disconnect();
-		}
+                while ((len = is.read(buf)) != -1) {
+                    baos.write(buf, 0, len);
+                }
+                baos.flush();
+                return baos.toString();
+            } else {
+                throw new CommonException("主人，我找不到服务器啦~");
+            }
 
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new CommonException("主人，我找不到服务器啦~");
+        } finally {
+            try {
+                if (is != null)
+                    is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                if (baos != null)
+                    baos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            conn.disconnect();
+        }
+
+    }
 
 }
